ddeefa1b536aa94d8a0500109417c31bb0ec4d97
scriptonist <aravind.techbridge@gmail.com>
2016-10-13 19:52:53 +0530

    Corrected Errors

--- app.js
+++ app.js
@@ -2,17 +2,11 @@
 var restify = require('restify'); 
 var builder = require('botbuilder'); 
 
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function() 
-{
-   console.log('%s listening to %s', server.name, server.url); 
-});
 
 // Create chat bot
 var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
+
 
 // Create bot dialogs
 bot.dialog('/', function (session) {
